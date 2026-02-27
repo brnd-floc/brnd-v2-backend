62e15b8c4d7dbfc6380c0c3576942b72b815130c
Jesús Ferrer Carrión <gsus@Mac-mini-de-Jesus.local>
2026-02-27 01:01:31 +0100

    fix: import AuthModule in EmbedsModule for AdminGuard DI

--- src/core/embeds/embeds.module.ts
+++ src/core/embeds/embeds.module.ts
@@ -3,12 +3,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminGuard, DebugEndpointGuard } from '../../security/guards';
+import { AuthModule } from '../auth/auth.module';
 import { EmbedsController } from './embeds.controller';
 import { EmbedsService, PodiumService } from './services';
 import { UserBrandVotes, User, Brand } from '../../models';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserBrandVotes, User, Brand])],
+  imports: [TypeOrmModule.forFeature([UserBrandVotes, User, Brand]), AuthModule],
   controllers: [EmbedsController],
   providers: [EmbedsService, PodiumService, AdminGuard, DebugEndpointGuard],
   exports: [EmbedsService, PodiumService],
